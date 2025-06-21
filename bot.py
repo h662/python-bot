4fa7eae50b6990d6c89765933233fd3c4d96820e
h662 <h662hong@gmail.com>
2025-06-21 11:05:44 +0900

    feat: setting

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+
+intents = discord.Intents.default()
+intents.message_content = True 
+intents.members = True 
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
+@bot.command(name="ping")
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
