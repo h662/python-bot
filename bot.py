b8d14ba47cac284e49bd39673a99fa1ac5e0483e
h662 <h662hong@gmail.com>
2025-06-21 14:10:33 +0900

    feat: scheduler

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from scheduler import setup_scheduler
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -17,6 +18,9 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("------")
+    # 실제 채널 ID로 교체
+    CHANNEL_ID = 1231937037532270657
+    setup_scheduler(bot, CHANNEL_ID)
 
 @bot.command(name="ping")
 async def ping(ctx):
